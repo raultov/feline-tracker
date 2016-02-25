4432f5e61e8bc75ae5d37f992f36a751fb4b570d
raultov <angel.raul.tovar.martin@gmail.com>
2016-02-25 22:47:28 +0100

    First commit

---
+++ src/test/java/com/ayoza/feline/FelineTrackerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ayoza.feline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FelineTrackerApplication.class)
+@WebAppConfiguration
+public class FelineTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
