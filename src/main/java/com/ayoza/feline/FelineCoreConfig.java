fc9982982425b5974d05454dbe77bb7d02fb648a
raultov <angel.raul.tovar.martin@gmail.com>
2016-04-17 22:37:12 +0200

    UserManager integration

---
+++ src/main/java/com/ayoza/feline/FelineCoreConfig.java
@@ -0,0 +1,11 @@
+package com.ayoza.feline;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = {"com.ayoza.feline.core"} , excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.ayoza.feline.core.test.*"))
+public class FelineCoreConfig {
+
+}
