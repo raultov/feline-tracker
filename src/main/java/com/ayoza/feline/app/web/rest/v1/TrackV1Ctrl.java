ae6aa7a2b8f22f51919195fff92bc5f5718749e8
raultov <angel.raul.tovar.martin@gmail.com>
2016-06-06 23:49:49 +0200

    NamedQuery working

--- src/main/java/com/ayoza/feline/app/web/rest/v1/TrackV1Ctrl.java
+++ src/main/java/com/ayoza/feline/app/web/rest/v1/TrackV1Ctrl.java
@@ -2,14 +2,17 @@ package com.ayoza.feline.app.web.rest.v1;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
 
 import ayoza.com.feline.api.entities.tracker.ApiTraRoute;
 import ayoza.com.feline.api.managers.tracker.TrackerMgr;
+import ayoza.com.feline.api.utils.DateUtils;
 
 @RestController
 @RequestMapping(value = "/v1/tracks")
@@ -25,8 +28,19 @@ public class TrackV1Ctrl {
 	@RequestMapping(value = "", method = POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, headers="Accept=*/*")
     @ResponseBody
     public ApiTraRoute addPointV1() {
-    	
-		ApiTraRoute route = trackerMgr.createRoute(1);
+		
+		
+		
+		Date limit = DateUtils.addMinutes2FechaActual(-1);
+		
+		ApiTraRoute route = trackerMgr.getCurrentRoute(1, limit);
+		
+		if (route == null) {
+			route = trackerMgr.createRoute(1);
+		}
+		
+		// TODO Add point to route
+		
 
     	return route;
 	}
