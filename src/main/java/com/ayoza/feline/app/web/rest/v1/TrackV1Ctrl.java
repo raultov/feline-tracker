b3937edeec44928fef812843bcbdc372745c2f65
raultov <angel.raul.tovar.martin@gmail.com>
2016-06-07 22:05:21 +0200

    AccessControl implemented

--- src/main/java/com/ayoza/feline/app/web/rest/v1/TrackV1Ctrl.java
+++ src/main/java/com/ayoza/feline/app/web/rest/v1/TrackV1Ctrl.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ayoza.feline.app.web.rest.v1.access.AccessControl;
+
+import ayoza.com.feline.api.entities.common.ApiUser;
 import ayoza.com.feline.api.entities.tracker.ApiTraRoute;
 import ayoza.com.feline.api.managers.tracker.TrackerMgr;
 import ayoza.com.feline.api.utils.DateUtils;
@@ -20,23 +23,27 @@ public class TrackV1Ctrl {
 	
 	private TrackerMgr trackerMgr;
 	
+	private AccessControl accessControl;
+	
 	@Autowired
-	public TrackV1Ctrl(TrackerMgr trackerMgr) {
+	public TrackV1Ctrl(TrackerMgr trackerMgr,
+						AccessControl accessControl) {
 		this.trackerMgr = trackerMgr;
+		this.accessControl = accessControl;
 	}
 	
 	@RequestMapping(value = "", method = POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, headers="Accept=*/*")
     @ResponseBody
     public ApiTraRoute addPointV1() {
 		
-		
+		ApiUser apiUser = accessControl.getUserFromSecurityContext();
 		
 		Date limit = DateUtils.addMinutes2FechaActual(-1);
 		
-		ApiTraRoute route = trackerMgr.getCurrentRoute(1, limit);
+		ApiTraRoute route = trackerMgr.getCurrentRoute(apiUser.getUserId(), limit);
 		
 		if (route == null) {
-			route = trackerMgr.createRoute(1);
+			route = trackerMgr.createRoute(apiUser.getUserId());
 		}
 		
 		// TODO Add point to route
---
+++ src/main/java/com/ayoza/feline/app/web/rest/v1/access/AccessControl.java
@@ -0,0 +1,49 @@
+package com.ayoza.feline.app.web.rest.v1.access;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import ayoza.com.feline.api.entities.common.ApiUser;
+import ayoza.com.feline.api.exceptions.UserServicesException;
+import ayoza.com.feline.api.managers.UserServicesMgr;
+
+@Service
+public class AccessControl {
+	
+	private UserServicesMgr userServicesMgr;
+	
+	@Autowired
+	public AccessControl(UserServicesMgr userServicesMgr) {
+		this.userServicesMgr = userServicesMgr;
+	}
+
+	public ApiUser getUserFromSecurityContext() {
+        
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+    	if ((authentication == null) || (authentication.getPrincipal() == null)) {
+    		return null;
+    	}
+		
+    	String username = null;
+    	if (authentication.getPrincipal() instanceof String) {
+    		username = (String) authentication.getPrincipal();
+    	} else {
+    		final UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+    		username = userDetails.getUsername();
+    	}
+        
+    	ApiUser user;
+		try {
+			user = userServicesMgr.getApiUserByUsername(username);
+		} catch (UserServicesException e) {
+			return null;
+		}
+		
+        return user;
+    }
+	
+}
