034cc4187daa294c977882420962df55030d5574
raultov <angel.raul.tovar.martin@gmail.com>
2016-07-04 17:29:13 +0200

    new method getAppUserV1

--- src/main/java/com/ayoza/feline/app/web/rest/v1/UserV1Ctrl.java
+++ src/main/java/com/ayoza/feline/app/web/rest/v1/UserV1Ctrl.java
@@ -2,13 +2,18 @@ package com.ayoza.feline.app.web.rest.v1;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ayoza.feline.app.web.rest.v1.access.AccessControl;
+
 import ayoza.com.feline.api.entities.app.ApiAppUser;
+import ayoza.com.feline.api.entities.common.ApiUser;
 import ayoza.com.feline.api.exceptions.FelineApiException;
+import ayoza.com.feline.api.exceptions.UserServicesException;
 import ayoza.com.feline.api.managers.UserServicesMgr;
 
 @RestController
@@ -16,15 +21,28 @@ import ayoza.com.feline.api.managers.UserServicesMgr;
 public class UserV1Ctrl {
 	
 	private UserServicesMgr userServicesMgr;
+	private AccessControl accessControl;
 
-	public UserV1Ctrl(UserServicesMgr userServicesMgr) {
+	@Autowired
+	public UserV1Ctrl(UserServicesMgr userServicesMgr,
+						AccessControl accessControl) {
 		this.userServicesMgr = userServicesMgr;
+		this.accessControl = accessControl;
 	}
 	
 	@RequestMapping(value = "", method = GET, produces = MediaType.APPLICATION_JSON_VALUE, headers="Accept=*/*")
     @ResponseBody
-    public ApiAppUser getUserV1() throws FelineApiException {
+    public ApiAppUser getAppUserV1() throws FelineApiException {
 		
-		return null;
+		ApiUser apiUser = accessControl.getUserFromSecurityContext();
+		
+		ApiAppUser apiAppUser = userServicesMgr.getApiUserByUserId(apiUser.getUserId());
+		if (apiAppUser == null) {
+			throw new UserServicesException(UserServicesException.ERROR_USER_NOT_FOUND, 
+											UserServicesException.ERROR_USER_NOT_FOUND_MSG, 
+											new Exception(UserServicesException.ERROR_USER_NOT_FOUND_MSG));
+		}
+		
+		return apiAppUser;
 	}
 }
