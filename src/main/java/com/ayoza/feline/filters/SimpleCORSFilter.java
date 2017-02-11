b8eb09222389531bac9046ce97d57ec131c37282
raultov <angel.raul.tovar.martin@gmail.com>
2017-02-11 20:42:51 +0100

    cleanup

--- src/main/java/com/ayoza/feline/filters/SimpleCORSFilter.java
+++ src/main/java/com/ayoza/feline/filters/SimpleCORSFilter.java
@@ -19,33 +19,27 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCORSFilter implements Filter {
 
-
 	public SimpleCORSFilter() {
 	}
 
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-	
-	    HttpServletRequest request = (HttpServletRequest) req;
-	   // String url = getFullURL(request);
-	   // if (url.startsWith("/oauth/token", url.length() - 12)) {
-	
-		    HttpServletResponse response = (HttpServletResponse) res;
-		
-		    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-		    response.setHeader("Access-Control-Allow-Credentials", "true");
-		    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-		    response.setHeader("Access-Control-Max-Age", "10");
-		    response.setHeader("Access-Control-Allow-Headers", "Content-Type,Accept,X-Requested-With,Authorization");
-		    
-		    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-		        response.setStatus(HttpServletResponse.SC_OK);
-		    } else {
-		        chain.doFilter(req, res);
-		    }
-	    /*} else {
-	    	chain.doFilter(req, res);
-	    }*/
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "10");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type,Accept,X-Requested-With,Authorization");
+
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, res);
+		}
 	}
 
 	@Override
@@ -55,15 +49,4 @@ public class SimpleCORSFilter implements Filter {
 	@Override
 	public void destroy() {
 	}
-/*
-	private static String getFullURL(HttpServletRequest request) {
-	    StringBuffer requestURL = request.getRequestURL();
-	    String queryString = request.getQueryString();
-
-	    if (queryString == null) {
-	        return requestURL.toString();
-	    } else {
-	        return requestURL.append('?').append(queryString).toString();
-	    }
-	}*/
 }
